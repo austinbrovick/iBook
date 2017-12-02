bf69897330fbaedccf8f6321a3f669de460ba0cc
Arden Kim <kmk3170@uw.edu>
2017-12-02 11:06:55 -0800

    switch and file type picker added to settings screen

--- iBook/Controller/SettingsViewController.swift
+++ iBook/Controller/SettingsViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var bookFileType: UITextField!
+    
+    let fileTypes = ["html", "kindle", "epub", "pdf"]
+    
+    var pickerView = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        bookFileType.inputView = pickerView
+        bookFileType.textAlignment = .center
+        bookFileType.placeholder = "Select type"
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +32,22 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fileTypes.count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fileTypes[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        bookFileType.text = fileTypes[row]
+        bookFileType.resignFirstResponder()
+    }
     /*
     // MARK: - Navigation
 
