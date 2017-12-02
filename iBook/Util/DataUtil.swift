fcd7ca8df78b1458db7d491380c83e7601d41c96
zachbryant <zbryant832@gmail.com>
2017-12-02 11:24:39 -0800

    Initial commit

---
+++ iBook/Util/DataUtil.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  iBook
+//
+//  Created by iGuest on 12/2/17.
+//  Copyright © 2017 Austin Brovick . All rights reserved.
+//
+
+import Foundation
+
+class DataUtil {
+    let file_user_history = ""
+    let file_user_challenge = ""
+    let file_categories = ""
+    let file_subCategories = ""
+    let api_openlib = "http://openlibrary.org/search.json?"
+    let api_openlib_author = "author=%s&"
+    let api_openlib_general = "q=%s"
+    let api_openlib_title = "title=%s%"
+    let api_openlib_page = "page=%d&"
+    
+    func getBook() {
+        
+    }
+    
+    func searchBook(){
+        
+    }
+    
+    func saveData(file to: String, data: String) {
+        
+    }
+    
+    func saveData(file to: String, data: Data) {
+        
+    }
+}
