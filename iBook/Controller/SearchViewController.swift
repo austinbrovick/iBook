4b71b8c698df9b2bbbb321887160892c79964a8b
Austin Brovick <austinbrovick@gmail.com>
2017-12-02 11:19:48 -0800

    Added search

--- iBook/Controller/SearchViewController.swift
+++ iBook/Controller/SearchViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    var data: [String: Any]?
+    
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("hello")
+        let input = searchBar.text!
+        getBook(input)
+    }
+    
+    func getBook(_ title: String) {
+        
+        
+        do {
+            var content: NSData?
+            if (title != "") {
+                let stringWithNoSpaces = title.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
+                let urlString = "https://openlibrary.org/search.json?q=\(stringWithNoSpaces)"
+                print(urlString)
+                let url = URL(string: urlString)!
+                content = NSData(contentsOf: url)
+            }
+            if (content == nil) {
+                print("no content")
+            } else {
+                data = try JSONSerialization.jsonObject(with: content as! Data, options: []) as! [String:Any]
+                print(data)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchBar.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
